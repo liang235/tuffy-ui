8a78e97f6d529653f38c5843736a3b7e6ec6c1cb
chen <13439594353@163.com>
2023-08-15 12:47:02 +0800

    更新

--- .vitepress/sidebar/card-page.mjs
+++ .vitepress/sidebar/card-page.mjs
@@ -9,9 +9,9 @@ export default [
 		collapsible: false, // 是否可折叠的侧边栏组
 		collapsed: false, // 是否展开侧边栏组，默认展开，设置为 true 为隐藏
 		items: [
-			{ text: '介绍', link: `${commonPath}/index` },
-			{ text: '表单数据', link: `${commonPath}/data` },
-			{ text: 'methods', link: `${commonPath}/methods` },
+			{ text: '介绍', link: `${commonPath}/index.md` },
+			{ text: '表单数据', link: `${commonPath}/data.md` },
+			{ text: 'methods', link: `${commonPath}/methods.md` },
 		],
 	},
 ]
--- .vitepress/sidebar/guide.mjs
+++ .vitepress/sidebar/guide.mjs
@@ -9,12 +9,12 @@ export default [
 		collapsible: false, // 是否可折叠的侧边栏组
 		collapsed: false, // 是否展开侧边栏组，默认展开，设置为 true 为隐藏
 		items: [
-			{ text: '基础页面', link: `${commonPath}/base-page` },
-			{ text: '自定义页面', link: `${commonPath}/custom-page` },
-			{ text: '页面属性', link: `${commonPath}/attributes`},
-			{ text: '模板引用', link: `${commonPath}/template-refs`},
-			{ text: '日期工具', link: `${commonPath}/date` },
-			{ text: 'utils 工具类', link: `${commonPath}/utils` },
+			{ text: '基础页面', link: `${commonPath}/base-page.md` },
+			{ text: '自定义页面', link: `${commonPath}/custom-page.md` },
+			{ text: '页面属性', link: `${commonPath}/attributes.md`},
+			{ text: '模板引用', link: `${commonPath}/template-refs.md`},
+			{ text: '日期工具', link: `${commonPath}/date.md` },
+			{ text: 'utils 工具类', link: `${commonPath}/utils.md` },
 		],
 	},
 ]
